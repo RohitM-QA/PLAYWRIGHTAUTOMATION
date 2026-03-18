edf405c1d4d9366fe51ca7ddc9692c9c6a396c1e
Rohit Mandal <roheeetmandal@gmail.com>
2026-03-18 22:31:40 +0545

    Initial commit - Playwright Java project

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,17 @@
+const {test} = require('@playwright/test');
+
+test('Browser Context Playwright Test', async({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+});
+
+test.only('Page Playwright Test', async({page})=>
+{
+    //const context = await browser.newContext();  --not required
+    //const page = await context.newPage();  --not required
+    await page.goto("https://google.com/");
+
+});
