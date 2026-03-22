b113c0d5cd968ebc5a70dade90ca5300fd5f4c8f
Rohit Mandal <roheeetmandal@gmail.com>
2026-03-22 21:28:25 +0545

    Added UI Control Code

---
+++ tests/UiControls.spec.js
@@ -0,0 +1,22 @@
+const {test, expect} = require('@playwright/test');
+
+test('UI Controls', async({page})=>
+{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const dropdown = page.locator("select.form-control");
+
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    
+    
+    
+    await page.pause();
+    
+ 
+
+    
+
+});
