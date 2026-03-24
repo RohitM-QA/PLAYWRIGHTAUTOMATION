6ee4ddff14723b6217a036573b83b8b294a2e1c2
Rohit Mandal <roheeetmandal@gmail.com>
2026-03-24 22:06:03 +0545

    Learned about how to Add Products and Check the Added Product

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,19 +1,38 @@
 const {test, expect} = require('@playwright/test');
 
-test('Browser Context Playwright Test', async({page})=>
+test('Client App Login', async({page})=>
 {
+    const productName = "ZARA COAT 3";   //product name to be added in the cart
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client/");
     await page.locator("#userEmail").fill("rohit.mandal2020@gmail.com");
     await page.locator("#userPassword").fill("Nepolic@7");
     await page.locator("#login").click();
-    //await page.waitForLoadState('networkidle');
+    await page.waitForLoadState('networkidle');
     await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
-    // const productName = "ZARA COAT 3";
-    // const productCount = await products.count();
-
+    
     console.log(titles);
-    
 
+    const count = await products.count();   //counting the number of products in the page
+    for(let i=0; i<count; ++i)
+    {
+        if(await products.nth(i).locator("b").textContent() === productName) //checking for the product name
     
+    {
+        await products.nth(i).locator('button:has-text(" Add To Cart")').click(); //clicking on add to cart button
+        break;
+    }  
+}    
+    await page.locator("[routerlink*='cart']").click(); //clicking on cart
+    await page.locator("div li").first().waitFor(); //waiting for the product to be visible in the cart
+
+    const isProductVisible = await page.locator("h3:has-text('ZARA COAT 3')").isVisible(); //waiting for the product to be visible in the cart, and this not a css
+    expect(isProductVisible).toBeTruthy(); //asserting that the product is visible in the cart
+
+
+
+
 });
+
+ 
