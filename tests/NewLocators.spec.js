e6e01acc3d100129990f4133e8f50c7cf92d6119
Rohit Mandal <rohit.percoidit@gmail.com>
2026-03-26 12:14:55 +0545

    Added New File

---
+++ tests/NewLocators.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Special Locators', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+
+
+});
